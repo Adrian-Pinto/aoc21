a401f87dd980d25ce336211254cab6d792845fd0
Adrian Pinto <adripintogonzalez@gmail.com>
2021-12-09 05:11:28 +0100

    Solve part 2 of day 1

--- Day01/main.js
+++ Day01/main.js
@@ -1,10 +1,13 @@
-const { readFileSync } = require('fs');
-const { stdout: output } = require('process');
+import { readFileSync } from 'fs';
+import { stdout as output } from 'process';
 
-const mesures = readFileSync(process.argv[2]).toString().split('\n');
-
-const result = mesures.reduceRight((amount, mesure, i) => (
-  i && amount + Number.parseInt(mesure / mesures[i - 1], 10)) || amount, 0);
-
+const result = readFileSync(process.argv[2])
+  .toString()
+  .split('\n')
+  .map((mesure, i, mesures) => (
+    i < mesures.length - 2 && +mesure + +mesures[i + 1] + +mesures[i + 2]) || 0)
+  .reduceRight((amount, mesure, i, mesures) => (
+    i && mesure !== mesures[i - 1] && amount + Number.parseInt(mesure / mesures[i - 1], 10)) || amount, 0);
+    
 output.write('\x1Bc');
 output.write(`Number of times mesures increases is: ${result}\n`);
