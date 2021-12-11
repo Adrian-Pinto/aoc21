844c12b71d86271aa2dd74bd545f836180477b53
Adrian Pinto <adripintogonzalez@gmail.com>
2021-12-11 08:00:20 +0100

    init npm and eslint add rules to eslintrc

--- Day03/main.js
+++ Day03/main.js
@@ -4,14 +4,15 @@
 
 // estados { media: inputLength / 2 }
 
-// gamma = input.reduce( (gamma, input) => 
+// gamma = input.reduce( (gamma, input) =>
 // input.split('').map((input, i) => parseInt(input) + gamma[i] )
-//,[] )
+// ,[] )
 // .map((value) => value > media ? 1 : 0 )
 // .joint('')
 
 // epsilon = gamma.split('').map((value) => parseInt(value, 2) ? 0 : 1  ).join('')
 
-// ! si sumo todos los bits positivos y son mayor a la mitad de filas el promedio sera positivo si no es negativo
+// ! si sumo todos los bits positivos y son mayor a la mitad de filas
+// ! el promedio sera positivo si no es negativo
 
 // stdout >> ("0b"+gamma) * ("0b"+epsilon)
