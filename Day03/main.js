d3068c26881c006b993e0da5d7ec07407e9a10e6
Adrian Pinto <adripintogonzalez@gmail.com>
2021-12-26 17:37:19 +0100

    solve part 2 isolate gamma logic to commonBit function add logic to the second map of commonBit function to chose the most common and uncommon bit add calculateRating recursive function for get oxygen and coTwo value

--- Day03/main.js
+++ Day03/main.js
@@ -2,23 +2,30 @@ import { readFileSync } from 'fs';
 import { stdout } from 'process';
 
 const input = readFileSync(process.argv[2]).toString().split('\n');
-const media = input.length / 2;
-const gamma = input
-  .reduce(
+
+const commonBit = (data, x = 1) => {
+  const media = data.length / 2;
+  return data.reduce(
     (acc, value) => (value ? value.split('')
       .map((digit, i) => (acc[i] === undefined ? digit : +acc[i] + +digit)) : acc),
-  ).map((value) => (value > media ? 1 : 0))
-  .join('');
+  ).map((value) => {
+    if (!x) return value < media ? 1 : value === media ? x : 0;
+    return value > media ? 1 : value === media ? x : 0;
+  }).join('');
+};
 
+const calculateRating = (data, x, bit = 0) => {
+  const media = commonBit(data, x);
+  const result = data.filter((value) => (value[bit] === media[bit]));
+  if (result.length === 1) return result;
+  return calculateRating(result, x, bit + 1);
+};
+
+const gamma = commonBit(input);
 const epsilon = gamma.split('').map((value) => (+value ? 0 : 1)).join('');
-
-// parametro de entrada x = 1 || 0
-// sumar todas las posiciones i y comprobar cual es el valor mas comun
-//   en el caso de que sean iguales decantarse por x
-// guardar las cadenas que coincidan con el valor mas comun
-// repetir con la siguiente posición asta que solo quede una cadena
-
-// life support rating = oxygen generator rating * CO2 scrubber rating
+const [oxygen] = calculateRating(input, 1);
+const [coTwo] = calculateRating(input, 0);
 
 stdout.write('\x1Bc');
 stdout.write(`Power consumption: ${`0b${gamma}` * `0b${epsilon}`}\n`);
+stdout.write(`Life support rating: ${`0b${oxygen}` * `0b${coTwo}`}\n`);
