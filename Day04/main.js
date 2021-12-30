80a9737e42f438e36c2e3d492a607858cb5fe8e8
Adrian Pinto <adripintogonzalez@gmail.com>
2021-12-30 17:48:19 +0100

    add columns how rows add pseudo code of solution

--- Day04/main.js
+++ Day04/main.js
@@ -2,15 +2,34 @@ import './src/chunkMethod.js';
 import { readFileSync } from 'fs';
 import { stdout } from 'process';
 
+const arrayColumn = (arr, n) => arr.map((x) => x[n]);
+
 const input = readFileSync(process.argv[2]).toString().replaceAll('\n\n', '\n').split('\n');
 
 const randomNum = input.shift(1).split(',');
-const bingoCards = input.map((value) => value.trim().replaceAll('  ', ' ').split(' ')).chunk(5);
-// value.replaceAll('  ', ' ')
+
+const bingoCards = input.map((row) => row.trim().replaceAll('  ', ' ').split(' ')).chunk(5)
+  .map((card) => {
+    const newCard = [...card];
+    card.forEach((row, i) => {
+      newCard.push(arrayColumn(card, i));
+    });
+    return newCard;
+  });
+
+// forEach random num
+//  const actualRandomNum = randomNum
+//  find n on all cards
+//    convert match string to num
+//    ?
+//    ? bingoCards[0][0][1] = Number.parseInt(bingoCards[0][0][1], 10);
+//    see if all i of a row are a typeof Num
+//      if true sum all i of a rom and * with actual random num
+//      return the result
 
 console.log(randomNum);
 console.log('---');
-console.log(bingoCards);
+console.log(bingoCards[0]);
 
 // stdout.write('\x1Bc');
 // stdout.write(input);
