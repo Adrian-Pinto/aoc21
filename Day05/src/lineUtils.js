1494288b421744c724d9d268d0dc7368b5a61c3e
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-09 20:19:31 +0100

    solve day5 part 2
    add genDiagonalCoors function on lineUtils
    add conditional in default funct in lineUtils for diagonal case

--- Day05/src/lineUtils.js
+++ Day05/src/lineUtils.js
@@ -1,9 +1,9 @@
 /**
- * @param {Array} set x,y coordinate
+ * @param {Array} set PointA[x,y] & PointB[x,y] coordinate
  * @param {Number} axis 0 for x || 1 for y
  * @returns {Array}
  */
-const sortCoor = (set, axis) => set.sort((x, y) => x[axis] - y[axis]);
+const sortCoor = (set, axis) => set.sort((a, b) => a[axis] - b[axis]);
 
 const genLineCoors = ([a, b], axis) => {
   const newCoors = [];
@@ -20,15 +20,34 @@ const genLineCoors = ([a, b], axis) => {
   return newCoors;
 };
 
+const genDiagonalCoors = ([a, b]) => {
+  const newCoors = [];
+  const dir = a[0] > b[0] ? 1 : 0;
+  if (dir) {
+    for (let i = a[1]; i <= b[1]; i++) {
+      newCoors.push([a[0] - i + a[1], a[1] + i - a[1]].join(','));
+    }
+  }
+  if (!dir) {
+    for (let i = a[1]; i <= b[1]; i++) {
+      newCoors.push([a[0] + i - a[1], a[1] + i - a[1]].join(','));
+    }
+  }
+  return newCoors;
+};
+
 export default (coorSet) => {
-  const or = [];
+  const extendedCoors = [];
   coorSet.forEach(([pointA, pointB]) => {
     if (pointA[1] === pointB[1] && pointA[0] !== pointB[0]) {
-      or.push(genLineCoors(sortCoor([pointA, pointB], 0), 0));
+      extendedCoors.push(genLineCoors(sortCoor([pointA, pointB], 0), 0));
     }
     if (pointA[0] === pointB[0] && pointA[1] !== pointB[1]) {
-      or.push(genLineCoors(sortCoor([pointA, pointB], 1), 1));
+      extendedCoors.push(genLineCoors(sortCoor([pointA, pointB], 1), 1));
+    }
+    if (pointA[0] !== pointB[0] && pointA[1] !== pointB[1]) {
+      extendedCoors.push(genDiagonalCoors(sortCoor([pointA, pointB], 1)));
     }
   });
-  return or;
+  return extendedCoors;
 };
