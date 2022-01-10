2a49684647cc108a1d627c3fd269587b0f4afd59
Adrian Pinto <adripintogonzalez@gmail.com>
2022-01-10 23:16:30 +0100

    solve part 1

--- Day06/main.js
+++ Day06/main.js
@@ -6,29 +6,18 @@ import {
   exit,
 } from 'process';
 import readline from 'readline';
+import fishCicle from './src/fishUtils.js';
 
 const [, , rawSample] = argv;
 
 const rl = readline.createInterface({ input, output });
 
-const fishSample = readFileSync(rawSample).toString().split(',')
+const fishSample = readFileSync(rawSample).toString().trim().split(',')
   .reduce((fishDays, fish) => {
     const accCopy = [...fishDays];
     accCopy[fish] = accCopy[fish] ? accCopy[fish] + 1 : 1;
     return accCopy;
-  }, [0, 0, 0, 0, 0, 0, 0, 0]);
-
-const fishCicle = (days, sample) => {
-  output.write('\x1Bc');
-  const fish = [...sample];
-  for (let i = 0; i < days; i++) {
-    // fish[0] <- cuantity of fish in 0
-    // rotateFishPos(fish, fish[0]) <- fish[0] is the new cuantity of fish[7]
-    // rotateFishPos -> all pos[i - 1]
-  }
-  console.log(fish);
-  exit(0);
-};
+  }, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
 const init = () => {
   output.write('\x1Bc');
